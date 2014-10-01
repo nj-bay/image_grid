f7e8329f4c7cc0d645caf01484d4f0bc33ec08ba
nj-bay <neal.jacob.415@gmail.com>
2014-09-30 20:10:18 -0700

    hubba hubba

--- image_grid.js
+++ image_grid.js
@@ -3,16 +3,23 @@ var mkdirp = require('mkdirp');
 var jade = require('jade');
 var fs = require('fs');
 
+// Image output size in pixel and JPEG quality
 var sizes = [400, 350, 300, 250, 200];
 var qualities = [100, 80, 60, 40, 20];
 
+// command line implementation at third argument
 var input_filename = process.argv[2];
 var output_folder = 'resized_images';
 
+// Create output folder (ignore if already exists)
 mkdirp.sync(output_folder);
 
 var file_sizes = {};
 var pending_images = 0;
+
+// Implement graphics magick to resize and compress image. Then write out the
+// file. If there are no errors, calculate file size of each and count down
+// until no more background processes are pending. Then invoke generate HTML
 
 var resize_and_compress = function (input_filename, size, quality)
 {
@@ -35,6 +42,7 @@ var resize_and_compress = function (input_filename, size, quality)
     pending_images += 1;
 }
 
+// Generate View with Jade Templating Engine
 var generate_html = function ()
 {
     console.log(file_sizes);
@@ -45,7 +53,7 @@ var generate_html = function ()
     var html_file = fs.openSync('index.html', 'w')
     fs.writeSync(html_file, html)
 }
-
+// Invoke resize_and_compress for each pair of size and quality nesting for loops
 sizes.forEach( function (size) {
     qualities.forEach( function (quality) {
         resize_and_compress(input_filename, size, quality);
